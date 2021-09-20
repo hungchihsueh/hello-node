ad4c312d8efdd8d829cc869078a5823a3077722b
hungchihsueh <chihsuehhung@gmail.com>
2021-09-20 10:47:28 +0800

    create sum.js

---
+++ practice/sum.js
@@ -0,0 +1,12 @@
+console.log("hello World");
+
+function sum(para){
+    total=(1+para)*para/2;
+    // for(i=0; i<para; i++){
+    //     total+=i;
+    // }
+    return total;
+}
+console.log(sum(3));
+console.log(sum(6));
+console.log(sum(10));
