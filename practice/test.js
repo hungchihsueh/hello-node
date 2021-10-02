caab2acdfa6cf810fa8675274e9a7e2ced978635
hungchihsueh <chihsuehhung@gmail.com>
2021-10-02 23:51:48 +0800

    Create test.js
    add test.js

---
+++ practice/test.js
@@ -0,0 +1,26 @@
+// function readData(idx) {
+//     for (let i = 0; i < 100; i++) {
+//       idx++;
+//       console.log(idx);
+//     }
+//     if (idx < 500) {
+//       readData(idx);
+//     }
+//   }
+  
+//   readData(0);
+//   console.log("after");
+function readData(idx) {
+  for (let i = 0; i < 100; i++) {
+    idx++;
+    console.log(idx);
+  }
+  if (idx < 500) {
+    setTimeout(function () {
+      readData(idx);
+    }, 0);
+  }
+}
+
+readData(0);
+console.log("after");
