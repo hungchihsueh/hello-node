43e78da74477ec41b5e9204d1d698d9745ce1c14
hungchihsueh <chihsuehhung@gmail.com>
2021-10-30 14:29:15 +0800

    連接設定資料庫

--- todos-be/server.js
+++ todos-be/server.js
@@ -1,32 +1,43 @@
 const express = require("express");
+require("dotenv").config();
+const mysql = require("mysql");
+const Promise = require("bluebird");
 
 let app = express();
 
-app.use((req, res, next) => {
-  console.log("hello!");
-  next();
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
-app.use((req, res, next) => {
-  let current = new Date();
-  console.log(`在${current}時有人
-  來訪!!!`);
-  next();
+connection = Promise.promisifyAll(connection);
+
+app.listen(3006, () => {
+  connection.connect();
+  console.log("express app 啟動!!!");
 });
 
-app.get("/", (req, res) => {
-  res.send("hello! home here.");
-});
-app.get("/member", (req, res) => {
-  res.send("hello! member here.");
+// app.use((req, res, next) => {
+//   console.log("hello!");
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   let current = new Date();
+//   console.log(`在${current}時有人
+//   來訪!!!`);
+//   next();
+// });
+
+app.get("/api/todos", async (req, res) => {
+  let data = await connection.queryAsync("SELECT * FROM todos");
+  res.json(data);
 });
 
 app.use((req, res, next) => {
   console.log("not found");
   res.status(404).send("fuck you!");
   next();
-});
-
-app.listen(3006, () => {
-  console.log("express app 啟動!!!");
 });
